3eb517c1c99099b219fc53e0588916df461df548
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-16 14:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homestate, training) {
     this.homestate = homestate;
     this.training = training;
     this.veto = function(){
-        console.log("No, I must disagree");
+        return "No, I must disagree";
     };
     this.approve = function(){
         return "You can do that!";
